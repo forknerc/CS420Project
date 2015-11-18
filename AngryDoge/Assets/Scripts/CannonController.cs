e8635e20bc5438f5f36ad74da62b9a497a5277e2
icet125 <icet125@gmail.com>
2015-11-17 16:55:56 -0800

    cannon controller

---
+++ AngryDoge/Assets/Scripts/CannonController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonController : MonoBehaviour {
+    public float rotationMultiplier;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rotationMultiplier = 2.0f;
+	}
+
+    public void rotateCannonUp()
+    {
+        transform.Rotate(Vector3.up * rotationMultiplier);
+    }
+
+    public void rotateCannonDown()
+    {
+        transform.Rotate(Vector3.down * rotationMultiplier);
+    }
+
+    public void rotateCannonLeft()
+    {
+        transform.Rotate(Vector3.left * rotationMultiplier);
+    }
+
+    public void rotateCannonRight()
+    {
+        transform.Rotate(Vector3.right * rotationMultiplier);
+    }
+}
