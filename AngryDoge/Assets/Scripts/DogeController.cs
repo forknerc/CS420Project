578ac30e2eabeee862ab7c1f21d8b94e32d4cdb5
Chris Forkner <cforkner@nevada.unr.edu>
2015-11-17 16:32:10 -0800

    added doge controller

---
+++ AngryDoge/Assets/Scripts/DogeController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogeController : MonoBehaviour 
+{
+    public int TimeUntilDespawn = 10;
+
+	// Use this for initialization
+	void Start () 
+	{
+        StartCoroutine(DogeTimeout());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+    private IEnumerator DogeTimeout()
+    {
+        yield return new WaitForSeconds(TimeUntilDespawn);
+
+        Destroy(gameObject);
+    }
+}
