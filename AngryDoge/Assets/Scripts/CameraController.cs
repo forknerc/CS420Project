eccc437d4976e655ccd0616bc60ad1daf2ace4be
Chris Forkner <cforkner@nevada.unr.edu>
2015-12-03 12:51:26 -0800

    Added camera controller and attached button click events for camera buttons

---
+++ AngryDoge/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CameraController : MonoBehaviour
+{
+    public List<Camera> GameCameras;
+
+    public void ChangeCamera(int camNum)
+    {
+        foreach(var cam in GameCameras)
+        {
+            cam.gameObject.SetActive(false);
+        }
+
+        GameCameras[camNum].gameObject.SetActive(true);
+    }
+}
